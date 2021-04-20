65d6884ce9580f3ff06b2792856273a899edf3c0
JP Quicksall <john.quicksall2@t-mobile.com>
2021-04-20 23:28:07 +0000

    Adding more doc strings

--- receiver/receiver.py
+++ receiver/receiver.py
@@ -1,9 +1,44 @@
 #!/usr/bin/env python
+"""Example Google style docstrings.
+
+This module demonstrates documentation as specified by the `Google Python
+Style Guide`_. Docstrings may extend over multiple lines. Sections are created
+with a section header and a colon followed by a block of indented text.
+
+Example:
+    Examples can be given using either the ``Example`` or ``Examples``
+    sections. Sections support any reStructuredText formatting, including
+    literal blocks::
+
+        $ python example_google.py
+
+Section breaks are created by resuming unindented text. Section breaks
+are also implicitly created anytime a new section starts.
+
+Attributes:
+    module_level_variable1 (int): Module level variables may be documented in
+        either the ``Attributes`` section of the module docstring, or in an
+        inline docstring immediately following the variable.
+
+        Either form is acceptable, but the two should not be mixed. Choose
+        one convention to document module level variables and be consistent
+        with it.
+
+Todo:
+    * For module TODOs
+    * You have to also use ``sphinx.ext.todo`` extension
+
+.. _Google Python Style Guide:
+   http://google.github.io/styleguide/pyguide.html
+
+"""
 import json
 import os
 import pika
 import logging
 import sys
+from typing import Dict, List, Any
+
 from time import sleep
 
 #For Logging
@@ -18,7 +53,7 @@ from time import sleep
 RMQ_EXCHANGE = os.getenv('RMQ_EXCHANGE', 'rabbitmq-demo')
 BINDING_KEY = os.getenv('BINDING_KEY', '#')
 
-def send_response(chan, props, body):
+def send_response(chan, props, body: Dict[str, Any]):
     """Function to finish the reply back to rabbitmq
     Args:
         chan (pika.channel.Channel): The string used for hashing
